4c2df1288ae89f163974e24b25cf009b4a3c1f64
Gustavo Duara <gugaduara@gmail.com>
2025-04-14 15:47:29 -0300

    criado classe amigo

---
+++ ClubeDaLeitura.ConsoleApp/Amigo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp
+{
+    public class Amigo
+    {
+        public int Id;
+        public string Name;
+        public string Responsavel;
+        public string Telefone;
+
+        public Amigo(int Id, string nome, string responsavel, string telefone) 
+        {
+            Id = Id;
+            Name = nome;
+            Responsavel = responsavel;
+            Telefone = telefone;
+        }
+
+
+    }
+}
