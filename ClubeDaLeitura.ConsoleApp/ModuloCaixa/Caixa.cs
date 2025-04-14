7769f7ad439d728bba0d07f5af8d4387925c8d42
Gustavo Duara <gugaduara@gmail.com>
2025-04-14 18:47:16 -0300

    criado modulo caixa

---
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
+{
+    public class Caixa
+    {
+        public int Id;
+        public string Etiqueta;
+        public string Cor;
+        public int Numero;
+
+        public Caixa(int id, string etiqueta, string cor, int numero)
+        {
+            Id = id;
+            Etiqueta = etiqueta;
+            Cor = cor;
+            Numero = numero;
+        }
+    }
+}
